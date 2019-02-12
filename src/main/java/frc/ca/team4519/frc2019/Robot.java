6f6c74ef675374a51daca8a77f970f90901af7a0
Connor <ceeman.adams@gmail.com>
2019-02-11 19:02:24 -0500

    more setup

--- src/main/java/frc/ca/team4519/frc2019/Robot.java
+++ src/main/java/frc/ca/team4519/frc2019/Robot.java
@@ -6,8 +6,24 @@ import frc.ca.team4519.lib.*;
 public class Robot extends MechaIterativeRobot{
 
 
-public void teleopPeriodic(){
+    public void autonomousInit() {
+
+    }
+
+    public void autonomousPeriodic() {
+
+    }
+
+    public void teleopInit() {
+
+    }
+
+    public void teleopPeriodic(){
     
-}
+    }
+
+    public void update() {
+        
+    }
 
 }
