762c5b5a14fbb8348895623daf6a14367ffb20e6
Connor <ceeman.adams@gmail.com>
2019-02-11 20:33:57 -0500

    The chronicles of Bob

--- src/main/java/frc/ca/team4519/frc2019/Constants.java
+++ src/main/java/frc/ca/team4519/frc2019/Constants.java
@@ -2,9 +2,13 @@ package frc.ca.team4519.frc2019;
 
 public class Constants {
 
+    //Drivebase
+        //Drive Motors
     public static final int leftDrive = 1;  //CONFIRMED
     public static final int rightDrive = 0; //CONFIRMED
-
+        //Shifter
+    public static final int shifter = 0;
+        //Encoders
     public static final int leftDriveEncoderA = 0;
     public static final int leftDriveEncoderB = 1;
     public static final int rightDriveEncoderA = 2;
@@ -12,4 +16,21 @@ public class Constants {
 
     public static final boolean isLeftDriveEncoderFlipped = false;
     public static final boolean isRihtDriveEncoderFlipped = false;
+
+    //Shooter
+    public static final int leftShooterWheel = 2;
+    public static final int rightShooterWheel = 3;
+    public static final int indexer = 1;
+
+    //Intake
+        //Solenoid
+    public static final int ian = 2;
+        //Intake
+    public static final int iansFriend = 4;
+
+    //Hatch Mech
+    public static final int harry = 3;
+    public static final int yeeter = 4;
+
+
 }
